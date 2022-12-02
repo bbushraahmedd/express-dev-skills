8a37113bbdd79fbb67a07edea1f2147bd7c0822b
bbushraahmedd <bushimed98@gmail.com>
2022-12-02 12:14:13 -0600

    add new skill

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,10 +2,12 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill
 }
 
 function index(req, res, next) {
+    console.log(req.test)
     const allSkills = Skill.getAll();
     res.render('skills/index', {skills: allSkills});
   }
@@ -16,4 +18,8 @@ function index(req, res, next) {
     // OR
     const oneSkill = Skill.getOne(req.params.id);
     res.render('skills/show', {skill: oneSkill});
+  }
+
+  function newSkill(req, res, next){
+    res.render('skills/new');
   }
--- routes/skills.js
+++ routes/skills.js
@@ -5,6 +5,7 @@ const skillController = require('../controllers/skills');
 
 /* GET users listing. */
 router.get('/', skillController.index);
+router.get('/new', skillController.new);
 router.get('/:id', skillController.show);
 
 module.exports = router;
